264d750b0ff1b21753ca4e7b3936534623c6ab83
Joss Mackison <joss.mackison@gmail.com>
2015-05-26 13:53:47 +1000

    Updating build

--- dist/react-blink.js
+++ dist/react-blink.js
@@ -1,9 +1,12 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Blink = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var React = require('react/addons');
+Object.defineProperty(exports, '__esModule', {
+	value: true
+});
+var blacklist = require('blacklist');
+var React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null);
 
 /*
  * Fuck yeah, blink tag!
@@ -12,6 +15,10 @@ var React = require('react/addons');
 var Blink = React.createClass({
 	displayName: 'Blink',
 
+	propTypes: {
+		children: React.PropTypes.element.isRequired,
+		duration: React.PropTypes.number
+	},
 	getInitialState: function getInitialState() {
 		return { visible: true };
 	},
@@ -27,19 +34,22 @@ var Blink = React.createClass({
 	componentWillUnmount: function componentWillUnmount() {
 		clearInterval(this.interval);
 	},
-
 	render: function render() {
-		var style = { visibility: this.state.visible ? 'visible' : 'hidden' };
+		var children = this.props.children;
 
+		var props = blacklist(this.props, 'children', 'duration');
+		props.style = { visibility: this.state.visible ? 'visible' : 'hidden' };
 		return React.createElement(
 			'span',
-			_extends({}, this.props, { style: style }),
-			this.props.children
+			props,
+			children
 		);
 	}
 });
 
-module.exports = Blink;
+exports['default'] = Blink;
+module.exports = exports['default'];
 
-},{"react/addons":undefined}]},{},[1])(1)
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"blacklist":undefined}]},{},[1])(1)
 });
